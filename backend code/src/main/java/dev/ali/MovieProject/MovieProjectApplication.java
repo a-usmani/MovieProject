06c15b96c0f05fe72108c85797f9728072769e74
a-usmani <105238278+a-usmani@users.noreply.github.com>
2023-08-16 03:14:05 -0400

    adding backend code

---
+++ backend code/src/main/java/dev/ali/MovieProject/MovieProjectApplication.java
@@ -0,0 +1,20 @@
+package dev.ali.MovieProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MovieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieProjectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "hello world";
+	}
+}
---
+++ backend code/src/test/java/dev/ali/MovieProject/MovieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ali.MovieProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
